01435046b0939bc2d8c4c3210645b9932284cb28
TEm <tem2k2.dev@gmail.com>
2024-12-01 17:09:52 +0700

    Connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './users/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
     controllers: [AppController, UserController],
     providers: [AppService],
 })
