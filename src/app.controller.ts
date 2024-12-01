bbe76a41af3892ed4713700ae2d31d4cedaac87f
TEm <tem2k2.dev@gmail.com>
2024-12-01 20:05:14 +0700

    ENV Variables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private configService: ConfigService,
+    ) {}
 
     @Get()
     @Render('home')
     getHello() {
         // return this.appService.getHello();
+        console.log(this.configService.get<string>('PORT'));
+
         const messageText = this.appService.getHello();
         return {
             message: messageText,
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './users/user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+    imports: [
+        // MongooseModule.forRoot('mongodb://localhost:27017'),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGO_URL'),
+            }),
+            inject: [ConfigService],
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
     controllers: [AppController, UserController],
     providers: [AppService],
 })
