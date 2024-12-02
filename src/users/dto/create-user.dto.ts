91d3af584ca8a07e6ba82b7fff418de19b01c96a
TEm <tem2k2.dev@gmail.com>
2024-12-02 17:28:43 +0700

    Validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,15 @@
 // Data transfer object
 
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Email không đúng định dạng' })
+    @IsNotEmpty({ message: 'Email không được bỏ trống' })
     email: string;
+
+    @IsNotEmpty({ message: 'Mật khẩu không được bỏ trống' })
     password: string;
+
     address: string;
     name: string;
 }
