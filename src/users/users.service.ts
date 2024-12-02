3c9b66a9bc58245a69c841489c1e9ee546ef6ed9
TEm <tem2k2.dev@gmail.com>
2024-12-03 05:04:27 +0700

    Get user by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
@@ -17,7 +17,6 @@ export class UsersService {
         return hash;
     };
 
-    // create(createUserDto: CreateUserDto);
     async create(info: CreateUserDto) {
         const hashPass = this.getHashPassword(info.password);
 
@@ -35,8 +34,25 @@ export class UsersService {
         return `This action returns all users`;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} user`;
+    async findOne(id: string) {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return { error: true, message: 'Id not valid' };
+        }
+
+        try {
+            // Tìm kiếm user với id
+            const user = await this.userModel.findOne({ _id: id }).exec();
+
+            // Kiểm tra nếu không tìm thấy user
+            if (!user) {
+                return { error: true, message: 'User not found' };
+            }
+
+            return user; // Trả về user nếu tìm thấy
+        } catch (error) {
+            console.error(error); // Ghi log lỗi ra console
+            return { error: true, message: error.message }; // Trả về lỗi
+        }
     }
 
     update(id: number, updateUserDto: UpdateUserDto) {
