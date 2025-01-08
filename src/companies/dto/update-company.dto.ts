8c308d6ecf252c1e8b10e37c0f2a1e653632446a
TEm <tem2k2.dev@gmail.com>
2025-01-08 23:48:40 +0700

    Update company

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCompanyDto } from './create-company.dto';
+import { IsNotEmpty } from 'class-validator';
 
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
+export class UpdateCompanyDto {
+    @IsNotEmpty({ message: 'Name không được bỏ trống' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Address không được bỏ trống' })
+    address: string;
+
+    @IsNotEmpty({ message: 'Description không được bỏ trống' })
+    description: string;
+}
