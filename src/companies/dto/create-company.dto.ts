d9a1e256d22abad59235e9c3a207c8c221511198
TEm <tem2k2.dev@gmail.com>
2025-01-08 20:05:39 +0700

    Generate modules Company

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCompanyDto {
+    @IsEmail({}, { message: 'Name không đúng định dạng' })
+    @IsNotEmpty({ message: 'Name không được bỏ trống' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Address không được bỏ trống' })
+    address: string;
+
+    @IsNotEmpty({ message: 'Description không được bỏ trống' })
+    description: string;
+}
---
+++ src/companies/schemas/company.schema.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type CompanyDocument = HydratedDocument<Company>;
+
+@Schema({ timestamps: true })
+export class Company {
+    @Prop()
+    name: string;
+
+    @Prop()
+    address: number;
+
+    @Prop()
+    phone: number;
+
+    @Prop()
+    description: string;
+
+    @Prop({type: Object})
+    createdBy: {
+        _id: mongoose.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop({type: Object})
+    updatedBy: {
+        _id: mongoose.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop({type: Object})
+    deletedBy: {
+        _id: mongoose.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+
+    @Prop()
+    isDeleted: Boolean;
+
+    @Prop()
+    deletedAt: Date;
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
