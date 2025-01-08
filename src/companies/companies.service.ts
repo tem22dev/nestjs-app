3fffc2286d9af170b049f02a456d58cf2d2fc139
TEm <tem2k2.dev@gmail.com>
2025-01-08 23:21:06 +0700

    Passing Req.user

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,16 +4,19 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class CompaniesService {
     constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) {}
 
-    async create(info: CreateCompanyDto) {
+    async create(info: CreateCompanyDto, user: IUser) {
         const company = await this.companyModel.create({
-            name: info.name,
-            address: info.address,
-            description: info.description,
+            ...info,
+            createdBy: {
+                _id: user._id,
+                email: user.email,
+            },
         });
 
         return company;
--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -1,4 +1,9 @@
-import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); // Định nghĩa thêm decorator để truyền thêm metadata vào handler
+
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+});
