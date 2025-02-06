408e46e7e5e947902e05399f4f8d59a928b556fb
TEm <tem2k2.dev@gmail.com>
2025-02-06 15:58:49 +0700

    Send mail with template

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,50 @@
+import { Controller, Get } from '@nestjs/common';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { MailerService } from '@nestjs-modules/mailer';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { SubscriberDocument } from 'src/subscribers/schemas/subscriber.schema';
+import { Subscriber } from 'rxjs';
+
+@Controller('mail')
+export class MailController {
+    constructor(
+        private readonly mailerService: MailerService,
+
+        @InjectModel(Job.name) private jobModel: SoftDeleteModel<JobDocument>,
+        @InjectModel(Subscriber.name) private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+    ) {}
+
+    @Get()
+    @Public()
+    @ResponseMessage('Test email')
+    async handleTestEmail() {
+        const subscribers = await this.subscriberModel.find({});
+        for (const subs of subscribers) {
+            const subsSkills = subs.skills;
+            const jobWithMatchingSkills = await this.jobModel.find({ skills: { $in: subsSkills } });
+
+            if (jobWithMatchingSkills?.length) {
+                const jobs = jobWithMatchingSkills.map((item) => {
+                    return {
+                        name: item.name,
+                        company: item.company.name,
+                        salary: `${item.salary}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' đ',
+                        skills: item.skills,
+                    };
+                });
+                await this.mailerService.sendMail({
+                    to: 'em21072006015@vnkgu.edu.vn',
+                    from: '"Support Team" <support@example.com>', // override default from
+                    subject: 'Welcome to Nice App! Confirm your Email',
+                    template: 'mail',
+                    context: {
+                        receiver: subs.name,
+                        jobs: jobs,
+                    },
+                });
+            }
+        }
+    }
+}
