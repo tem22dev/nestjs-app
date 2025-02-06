8a2635af66843df710ac2dcb2ae045e61dd4508e
TEm <tem2k2.dev@gmail.com>
2025-02-07 00:11:19 +0700

    Fix bug upload file and custom message error

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -10,33 +10,36 @@ import {
     UploadedFile,
     ParseFilePipeBuilder,
     HttpStatus,
+    UseFilters,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ResponseMessage } from 'src/decorator/customize';
+import { HttpExceptionFilter } from 'src/core/http-exception.filter';
 
 @Controller('files')
 export class FilesController {
     constructor(private readonly filesService: FilesService) {}
 
     @Post('upload')
+    @UseFilters(new HttpExceptionFilter())
     @ResponseMessage('Upload file')
     @UseInterceptors(FileInterceptor('file'))
     uploadFile(
         @UploadedFile(
-            new ParseFilePipeBuilder()
-                .addFileTypeValidator({
-                    fileType:
-                        /^(jpg|jpeg|image\/jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/,
-                })
-                .addMaxSizeValidator({
-                    maxSize: 1024 * 1024, // KB = 1024 MB = 1GB
-                })
-                .build({
-                    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-                }),
+            // new ParseFilePipeBuilder()
+            //     .addFileTypeValidator({
+            //         fileType:
+            //             /^(jpg|jpeg|image\/jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/,
+            //     })
+            //     .addMaxSizeValidator({
+            //         maxSize: 1024 * 1024, // KB = 1024 MB = 1GB
+            //     })
+            //     .build({
+            //         errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+            //     }),
         )
         file: Express.Multer.File,
     ) {
--- src/files/multer.config.ts
+++ src/files/multer.config.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { MulterModuleOptions, MulterOptionsFactory } from '@nestjs/platform-express';
 import fs from 'fs';
 import { diskStorage } from 'multer';
@@ -34,27 +34,59 @@ export class MulterConfigService implements MulterOptionsFactory {
                     break;
             }
         });
-    }
+    };
+
+    // createMulterOptions(): MulterModuleOptions {
+    //     return {
+    //         storage: diskStorage({
+    //             destination: (req, file, callback) => {
+    //                 const folder = req?.headers?.folder_type ?? 'default';
+    //                 this.ensureExists(`public/images/${folder}`);
+    //                 callback(null, join(this.getRootPath(), `public/images/${folder}`));
+    //             },
+    //             filename: (_req, file, callback) => {
+    //                 // get image extension
+    //                 let extName = path.extname(file.originalname);
+
+    //                 // get image's name (without extension)
+    //                 let baseName = path.basename(file.originalname, extName);
+
+    //                 let finalName = `${baseName}-${Date.now()}${extName}`;
+    //                 callback(null, finalName);
+    //             },
+    //         }),
+    //     };
+    // }
 
     createMulterOptions(): MulterModuleOptions {
         return {
             storage: diskStorage({
-                destination: (req, file, callback) => {
+                destination: (req, file, cb) => {
                     const folder = req?.headers?.folder_type ?? 'default';
                     this.ensureExists(`public/images/${folder}`);
-                    callback(null, join(this.getRootPath(), `public/images/${folder}`));
+                    cb(null, join(this.getRootPath(), `public/images/${folder}`));
                 },
-                filename: (_req, file, callback) => {
-                    // get image extension
+                filename: (req, file, cb) => {
+                    //get image extension
                     let extName = path.extname(file.originalname);
-
-                    // get image's name (without extension)
+                    //get image's name (without extension)
                     let baseName = path.basename(file.originalname, extName);
-
                     let finalName = `${baseName}-${Date.now()}${extName}`;
-                    callback(null, finalName);
+                    cb(null, finalName);
                 },
             }),
+            fileFilter: (req, file, cb) => {
+                const allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'];
+                const fileExtension = file.originalname.split('.').pop().toLowerCase();
+                const isValidFileType = allowedFileTypes.includes(fileExtension);
+
+                if (!isValidFileType) {
+                    cb(new HttpException('Invalid file type', HttpStatus.UNPROCESSABLE_ENTITY), null);
+                } else cb(null, true);
+            },
+            limits: {
+                fileSize: 1024 * 1024 * 1, // 1MB
+            },
         };
     }
 }
